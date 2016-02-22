85b1bae021d5e329e481840e1a76d727fbc3b901
Abhinaba Basu <abhinaba@gmail.com>
2016-02-21 16:06:54 -0800

    List arduino boards

---
+++ Arduino/ListBoard.ps1
@@ -0,0 +1,18 @@
+﻿
+$f = Get-ChildItem -Path $args[0] -Filter "boards.txt" -Recurse
+foreach($file in $f)
+{
+    Write-Host "For file" $file.FullName
+    
+    foreach ($l in get-content $file.FullName) {
+        if($l.Contains(".name")) {
+            $b = $l.Split('=')[1];
+        }
+
+        if($l.Contains(".board")) {
+            $s = [string]::Format("{0,-40}ARDUINO_{1}", $b, ($l.Split('=')[1]));
+            Write-Host $s
+        }
+
+    }
+}
