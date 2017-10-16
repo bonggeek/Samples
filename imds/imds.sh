26ff1b865b3e3f9c14e29b7d2c833fe4146d3c2f
BongGeek <abhinaba@gmail.com>
2017-10-16 20:25:36 +0000

    removed commented out code

--- imds/imds.sh
+++ imds/imds.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#sudo apt-get install curl
-#sudo apt-get jq
-#curl -H Metadata:True "http://169.254.169.254/metadata/instance?api-version=2017-04-02&format=json" | jq .
 
 getVmId()
 {
