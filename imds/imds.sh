2712670a5d95791bed823f21f15ac74cd86b351e
BongGeek <abhinaba@gmail.com>
2017-10-16 20:23:43 +0000

    updated imds shell script to get status code

--- imds/imds.sh
+++ imds/imds.sh
@@ -1,3 +1,23 @@
-sudo apt-get install curl
-sudo apt-get jq
-curl -H Metadata:True "http://169.254.169.254/metadata/instance?api-version=2017-04-02&format=json" | jq .
+#!/bin/bash
+#sudo apt-get install curl
+#sudo apt-get jq
+#curl -H Metadata:True "http://169.254.169.254/metadata/instance?api-version=2017-04-02&format=json" | jq .
+
+getVmId()
+{
+    for i in `seq 1 60`
+    do
+        retVal=`curl -H Metadata:True --silent --write-out "~%{http_code}" http://169.254.169.254/metadata/instance/compute/vmId?api-version=2017-04-02\&format=text`
+        IFS='~' read vmId http_status <<< $retVal
+        if [ "$http_status" == "200" ]; then
+            echo "Got vmId"
+            break
+        fi
+
+        echo "Attempt $i: Failed with $http_status ($vmId)"
+        sleep 1
+    done
+}
+getVmId
+echo $vmId
+
