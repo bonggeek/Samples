35472ece4b6dcc6ddc43c1bd8d89068db4d25cfb
Bonggeek <abhinaba@gmail.com>
2018-04-26 11:33:01 -0700

    hana sample

---
+++ hana.sh
@@ -0,0 +1,12 @@
+$ az extension add -y --source https://github.com/Azure/azure-hanaonazure-cli-extension/releases/download/0.1.0/azure_cli_hanaonazure_ext-0.1.0-py2.py3-none-any.whl
+$ az extension add -y --name azure-cli-hanaonazure-ext
+
+Subscription: 
+$ az login 
+$ az account set -s 
+$ az hanainstance list
+$ az hanainstance show --resource-group SJCT-T050 --instance-name azlinux0033
+
+SubID: 1ba60399-6ce1-4704-8155-5921d35e914e
+Region: eastus2euap
+
